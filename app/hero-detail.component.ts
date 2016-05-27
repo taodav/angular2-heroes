19e97fcedde02a2041f7863675f1f889548e2a1d
Bill & David Tao <pair+billdevcode.taodav@devbootcamp.com>
2016-05-26 20:29:44 -0700

    Add working services

---
+++ app/hero-detail.component.ts
@@ -0,0 +1,22 @@
+import {Component, Input} from '@angular/core'
+import {Hero} from './hero'
+
+@Component({
+  selector: 'my-hero-detail',
+  template: `
+    <div *ngIf="hero">
+    <h2>{{hero.name}} details!</h2>
+    <div>
+    <label>id: </label>{{hero.id}}
+    <input [(ngModel)]="hero.name" placehold="name">
+    </div>
+  </div>
+  `
+
+})
+
+export class HeroDetailComponent {
+  @Input()
+  hero: Hero
+
+}
